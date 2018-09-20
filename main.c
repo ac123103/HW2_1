a702be5b10c1d3b43618e5a03214fdd70da869dc
Andrea Ford <ac123103@ohio.edu>
2018-09-20 11:06:43 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   // printf("Hello world!\n");
+   int x = 39;
+   int y = x+3;
+   int z = y+x;
+   printf("%d,%d,%d",x,y,z);
+    return 0;
+}
